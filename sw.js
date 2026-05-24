312d17f59bedaafa48658aeab7c137283f67f9d2
Collazos022 <114321975+Collazos022@users.noreply.github.com>
2026-05-24 12:01:46 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'sb-admin-cache-v1';
+const CACHE_NAME = 'sb-admin-cache-v2';
 const urlsToCache = [
   './',
   './index.html',
-  './style.css',
-  './app.js',
+  './style.css?v=2.5',
+  './app.js?v=2.5',
   './SB_V.1.ico',
   './manifest.json',
   'https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@300;400;500;600;700;800&display=swap',
