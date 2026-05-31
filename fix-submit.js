67c80864aa4db4a112beeb672b0f38d1b2df0cd8
Usuario SketchUp <usuario@sketchup.local>
2026-05-31 02:04:35 -0500

    chore: clean up repository by deleting old debug scripts and test files

--- fix-submit.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-let appjs = fs.readFileSync('app.js', 'utf8');
-
-appjs = appjs.replace(/fetch\(APP_URL,/g, 'fetch(API_URL,');
-appjs = appjs.replace(/loadDashboardData\(\);/g, 'fetchData();');
-
-fs.writeFileSync('app.js', appjs);
-console.log("Fixed typos in btn-submit-expenses");
--- force_title.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-let appjs = fs.readFileSync('app.js', 'utf8');
-
-appjs = appjs.replace(/'register': 'Registrar Gastos'/g, "'register': 'Registro de Gastos'");
-appjs = appjs.replace(/'register': 'Registrar Compras'/g, "'register': 'Registro de Gastos'");
-
-fs.writeFileSync('app.js', appjs);
-console.log("Title force updated");
